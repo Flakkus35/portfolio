e5ece8e3f91f8a4ab3b990dbcf3dc9bbe0318091
Devon Wood <wooddv35@gmail.com>
2019-09-27 15:48:29 -0400

    Changed bio text

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -5,21 +5,19 @@ function About() {
     <div className="content-section">
       <div className="about-section">
         <p>
-          Hello, my name is Devon Wood and I am front end developer working primarily with
-          Javascript, HTML, and CSS. I have spent most of my life surrounded by computers have
-          enjoyed working with a multitude of different software in many fields. I taught myself
-          several 3D modeling and graphic design software starting from when I was 17 learning
-          programs like Autodesk Maya, Adobe Photoshop, and ZBrush to name a few. I have also worked
-          with several game engines including Unity and the Unreal Engine to make a few small
-          projects.
+          Hey all! My name is Devon Wood and I have been working in web development learning
+          languages such as Javascript, HTML, CSS, and SASS among others to create new and
+          interesting applications. While primarily working in frontend development I also have
+          experience working in backend technologies such as Express and Ruby on Rails in addition
+          to major databases like MySQL, Postgres and Mongo. I am currently working for Trilogy
+          Education helping new people enter into this industry.
         </p>
         <p>
-          I am and have always been a avid gamer ever since I first owned a PC. My very first and
-          favorite game was the original Doom and I could spend hours on end playing. I spent many
-          years learning game design and the intricate roles each member of a development team had
-          which led me to greatly respect the skill and effort of the people involved. I am
-          currently working for Trilogy Education tutoring students in the same class I went through
-          to help them better understand the world of coding.
+          I have spent most of my life surrounded by computers and have enjoyed working with a
+          multitude of different software in many fields. I taught myself several 3D modeling and
+          graphic design software starting from when I was 17 learning programs like Autodesk Maya,
+          Adobe Photoshop, and ZBrush to name a few. I have also worked with several game engines
+          including Unity and the Unreal Engine to make a few small projects.
         </p>
       </div>
     </div>
